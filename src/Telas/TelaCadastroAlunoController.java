83a1a4b5be9b056d3798c34489b8a1f36a1e1018
Kelvin Brancalhao <kelvinbrancalhao@gmail.com>
2016-06-03 02:30:46 -0300

    melhoria telas

--- src/Telas/TelaCadastroAlunoController.java
+++ src/Telas/TelaCadastroAlunoController.java
@@ -5,8 +5,24 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 public class TelaCadastroAlunoController implements Initializable {
+    
+    protected TextField TF_nome;
+    protected TextField TF_NID;
+    protected TextField TF_dataNascimento;
+    protected TextField TF_RG;
+    protected TextField TF_orgaoEmissor;
+    protected TextField TF_CPF;
+    protected TextField TF_celular;
+    protected TextField TF_Telefone;
+    protected TextField TF_endereco;
+    protected TextField TF_bairro;
+    protected TextField TF_numeroCasa;
+    protected ComboBox Combo_Sexo;
+    
 
 
     @Override
