dd627d4783de0ca457343fca26d44961311d6005
Kelvin Brancalhao <kelvinbrancalhao@gmail.com>
2016-06-03 08:06:53 -0300

    Alteração e chamada no menu

--- src/Telas/TelaCadastroAlunoController.java
+++ src/Telas/TelaCadastroAlunoController.java
@@ -1,27 +1,34 @@
 
 package Telas;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class TelaCadastroAlunoController implements Initializable {
     
-    protected TextField TF_nome;
-    protected TextField TF_NID;
-    protected TextField TF_dataNascimento;
-    protected TextField TF_RG;
-    protected TextField TF_orgaoEmissor;
-    protected TextField TF_CPF;
-    protected TextField TF_celular;
-    protected TextField TF_Telefone;
-    protected TextField TF_endereco;
-    protected TextField TF_bairro;
-    protected TextField TF_numeroCasa;
-    protected ComboBox Combo_Sexo;
+    public TextField TF_nome;
+    public TextField TF_NID;
+    public TextField TF_dataNascimento;
+    public TextField TF_RG;
+    public TextField TF_orgaoEmissor;
+    public TextField TF_CPF;
+    public TextField TF_celular;
+    public TextField TF_Telefone;
+    public TextField TF_endereco;
+    public TextField TF_bairro;
+    public TextField TF_numeroCasa;
+    public ComboBox Combo_Sexo;
     
 
 
@@ -31,13 +38,54 @@ public class TelaCadastroAlunoController implements Initializable {
     
     }
    
-    public void BotaoVoltar(ActionEvent event){
-        
-        
-    }
+    
     public void BotaoCancelar(ActionEvent event){
         
         
     }
+    
+    public void BotaoCadastrar(ActionEvent event){
+        
+        
+    }
+    
+    public void BotaoCadastrarTreino(ActionEvent event) throws IOException{
+         Parent telamenusparent = FXMLLoader.load(getClass().getResource("TelaCadastroTreino.fxml"));
+        Stage stage = new Stage();
+        Scene telamenusScene = new Scene(telamenusparent);
+        stage.setScene(telamenusScene);
+        stage.setTitle("tela");
+        stage.show();
+        
+    }
+    
+    public void BotaoCadastrarFicha(ActionEvent event) throws IOException{
+         Parent telamenusparent = FXMLLoader.load(getClass().getResource("TelaCadastroFisiologico.fxml"));
+        Stage stage = new Stage();
+        Scene telamenusScene = new Scene(telamenusparent);
+        stage.setScene(telamenusScene);
+        stage.setTitle("tela");
+        stage.show();
+        
+        
+    }
+    public void BotaoLimpar(ActionEvent event){
+        
+     TF_nome.setText("");
+     TF_NID.setText("");
+     TF_dataNascimento.setText("");
+     TF_RG.setText("");
+     TF_orgaoEmissor.setText("");
+     TF_CPF.setText("");
+     TF_celular.setText("");
+     TF_Telefone.setText("");
+     TF_endereco.setText("");
+     TF_bairro.setText("");
+     TF_numeroCasa.setText("");
+        
+    }
+    
+    
+    
     
 }
--- src/Telas/TelaMenusController.java
+++ src/Telas/TelaMenusController.java
@@ -1,12 +1,14 @@
 package Telas;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class TelaMenusController implements Initializable {
@@ -15,7 +17,20 @@ public class TelaMenusController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
       
+        
     }  
+    
+    public void MenuCadastroAluno(ActionEvent evento) throws IOException{
+        // sistema q deu certo
+        Parent telamenusparent = FXMLLoader.load(getClass().getResource("TelaCadastroAluno.fxml"));
+        Stage stage = new Stage();
+        Scene telamenusScene = new Scene(telamenusparent);
+        stage.setScene(telamenusScene);
+        stage.setTitle("tela");
+        stage.show();
+        
+        
+    }
     
   
     
